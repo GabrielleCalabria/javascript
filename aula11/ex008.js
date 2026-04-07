bbbf02ded98f0be6a67b34e4fb8bc738e3c30cbe
Gabrielle Calabria <201589061+GabrielleCalabria@users.noreply.github.com>
2026-04-07 13:56:53 +0100

    Function and Conditions
    JS Activities with:
    function ( )
    if (condition) {
    } else {
    }

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 65
+console.log(`A velocidade do seu carro é ${vel}km/h.`)
+if (vel > 60) { //Condicao simples
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de seguranca!')
