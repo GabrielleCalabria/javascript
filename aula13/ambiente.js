5e02a13298610dca84e2f172ff6e626f93edefe7
Gabrielle Calabria <201589061+GabrielleCalabria@users.noreply.github.com>
2026-04-10 13:05:15 +0100

    Repetition
    while
    do..while
    for

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ //c = c+1
+} while (c <= 6)
+
+/*
+var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++ //c = c+1
+}
+*/
+
+/* Atencao! Pior opcao para repeticao
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ aula14/ambiente.js
@@ -0,0 +1,27 @@
+/* Estrutura do..while
+var c = 1
+do {
+    console.log(c)
+    c++
+} while (c <=5)
+*/
+
+/* Estrutura while 
+var c = 1
+while (c <=5) {
+    console.log(c)
+    c++
+}
+*/
+
+/* Estrutura for
+for (c = 1; c <= 5; c++) {
+    console.log(c)
+}
+*/ 
+
+console.log('Vai comecar...')
+for (c = 1; c <= 4; c++) {
+    console.log(c)
+}
+console.log('FIM!')
